2b15ab599962faaf51e7c851deced6a7e6af9763
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-18 12:47:53 -0800

    push

---
+++ wp-content/themes/harris/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,127 @@
+<section id="section_four">
+	
+	<div class="sec_four_wave_top">
+		
+		<img class="wave" src="<?php bloginfo('template_directory');?>/images/cr-bg-top.svg"/>
+		
+	</div><!-- sec_four_wave_top -->
+	
+	<div class="sec_four_bg">
+		
+		<div class="sec_four_inner">
+			
+			<span class="sec_four_title">Case Results</span><!-- sec_four_title -->
+			
+			<a class="view_cr" href="">View All Results</a><!-- view_cr -->
+			
+			<div class="sec_four_cr_wrapper">
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+				
+				<div class="sec_four_cr">
+					
+					<div class="sec_four_reg">
+						
+						<img src="<?php bloginfo('template_directory');?>/images/cr-01-01.svg"/>
+						
+						<span class="amount">$4.95m</span>
+						
+						<span class="type">Motor Vehicle Accidents</span><!-- type -->
+						
+					</div><!-- sec_four_reg -->
+					
+						<div class="sec_four_hov"></div><!-- sec_four_hov -->
+					
+				</div><!-- sec_four_cr -->
+								
+			</div><!-- sec_four_cr_wrapper -->
+			
+		</div><!-- sec_four_inner -->
+		
+	</div><!-- sec_four_bg -->
+	
+	<div class="sec_four_wave_bottom">
+		
+		<img class="wave"  src="<?php bloginfo('template_directory');?>/images/cr-bg-bottom.svg"/>
+		
+	</div><!-- sec_four_wave_bottom -->
+	
+</section><!-- section_four -->
