5b5caefe906c0e981956c57da8b36d8392ab4855
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-18 16:51:24 -0800

    push

---
+++ wp-content/themes/harris/page-templates/homepage_template_parts/section-5.php
@@ -0,0 +1,43 @@
+<section id="section_five">
+	
+	<div class="sec_five_inner content">
+		
+		<div class="sec_five_left">
+			
+			<div class="sec_five_img">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/content-atts-02.png"/>
+				
+			</div><!-- sec_five_img -->
+			
+			<div class="sec_five_quote_wrapper">
+				
+				<div class="sec_five_quote">
+					
+					<span>You pay nothing unless we recover compensation on your behalf. <strong>Our fees are based on the success of your case.</strong></span>
+					
+				</div><!-- sec_five_quote -->
+				
+			</div><!-- sec_five_quote_wrapper -->
+			
+		</div><!-- sec_five_left -->
+		
+			<div class="sec_five_right">
+				
+				<span class="sec_five_header">let’s fight this</span><!-- sec_five_header -->
+				
+				<h2>H2 heading style dolor sit amet, constur adipisicing elit</h2>
+				
+				<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+				<h3>H3 heading style sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h3> 
+				
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+				
+			</div><!-- sec_five_right -->
+		
+	</div><!-- sec_five_inner -->
+	
+</section><!-- section_five -->
