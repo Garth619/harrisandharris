065a33306e5eff414e71bb18fb74c58562e62454
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-18 18:35:36 -0800

    push

---
+++ wp-content/themes/harris/page-templates/homepage_template_parts/section-6.php
@@ -0,0 +1,38 @@
+<section id="section_six">
+	
+	<div class="sec_six_wave_top">
+		
+		<img class="wave top" src="<?php bloginfo('template_directory');?>/images/test-bg-top.svg"/>
+		
+	</div><!-- sec_six_wave_top -->
+	
+	<div class="sec_six_bg">
+		
+		<div class="sec_six_inner">
+			
+			<span class="sec_six_title">Client Reviews</span><!-- sec_six_title -->
+			
+			<a class="view_cr top" href="">View All Results</a><!-- view_cr -->
+			
+			<div class="sec_six_test_wrapper">
+				
+				<div class="sec_six_test"></div><!-- sec_six_test -->
+				
+				<div class="sec_six_test"></div><!-- sec_six_test -->
+				
+				<div class="sec_six_test"></div><!-- sec_six_test -->
+				
+			</div><!-- sec_six_test_wrapper -->
+			
+		</div><!-- sec_six_inner -->
+	
+	</div><!-- sec_six_bg -->
+	
+	<div class="sec_six_wave_bottom">
+		
+		<img class="wave bottom"  src="<?php bloginfo('template_directory');?>/images/test-bg-bottom.svg"/>
+		
+	</div><!-- sec_six_wave_bottom -->
+	
+	
+</section><!-- section_six -->
