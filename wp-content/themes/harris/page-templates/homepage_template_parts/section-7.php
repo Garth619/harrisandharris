a6fe5d6e9926b390f7452c20fc578db332590b3d
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-19 18:19:19 -0800

    push

--- wp-content/themes/harris/page-templates/homepage_template_parts/section-7.php
+++ wp-content/themes/harris/page-templates/homepage_template_parts/section-7.php
@@ -1,5 +1,63 @@
 <section id="section_seven">
 	
-	text
+	<div class="sec_seven_inner">
+		
+		<span class="sec_seven_title">Practice Areas</span><!-- sec_seven_title -->
+		
+		<div class="sec_seven_content">
+			
+			<div class="sec_seven_left">
+				
+				<div class="sec_seven_pa">
+					
+					<div class="sec_seven_col">
+						
+						<ul>
+							<li><a href="">Car Accidents</a></li>
+							<li><a href="">Car Accidents</a></li>
+							<li><a href="">Car Accidents</a></li>
+						</ul>
+						
+					</div><!-- sec_seven_col -->
+					
+					<div class="sec_seven_col">
+							
+						<ul>
+							<li><a href="">Car Accidents</a></li>
+							<li><a href="">Car Accidents</a></li>
+							<li><a href="">Car Accidents</a></li>
+						</ul>
+							
+					</div><!-- sec_seven_col -->
+					
+				</div><!-- sec_seven_pa -->
+				
+				<a class="view_pa" href="">View All Practice Areas</a><!-- view_pa -->
+				
+			</div><!-- sec_seven_left -->
+			
+			<div class="sec_seven_right">
+				
+				<div class="sec_seven_img">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/pa-no-win-no-fee-img.png"/>
+					
+				</div><!-- sec_seven_img -->
+				
+				<div class="sec_seven_quote_wrapper">
+				
+					<div class="sec_seven_quote">
+					
+					<span>Personal injury isn’t just our specialty, <strong>it’s all we do.</strong></span>
+					
+					</div><!-- sec_seven_quote -->
+				
+			</div><!-- sec_seven_quote_wrapper -->
+				
+			</div><!-- sec_seven_right -->
+			
+		</div><!-- sec_seven_content -->
+		
+	</div><!-- sec_seven_inner -->
 	
 </section><!-- section_seven -->
