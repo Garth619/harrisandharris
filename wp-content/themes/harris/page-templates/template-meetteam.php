a898402558881e11b430ca7bd0278641297f8144
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-23 17:41:01 -0800

    push

--- wp-content/themes/harris/page-templates/template-meetteam.php
+++ wp-content/themes/harris/page-templates/template-meetteam.php
@@ -5,6 +5,23 @@
 get_header(); ?>
 
 
+<div id="internal_main">
 
+
+
+<div class="default_container">
+	
+		
+</div><!-- default_container -->
+
+
+</div><!-- internal_main -->
+		
+	<?php //get_template_part( 'loop', 'page' ); ?>
+		
+	
+	
+
+	
 
 <?php get_footer(); ?>
