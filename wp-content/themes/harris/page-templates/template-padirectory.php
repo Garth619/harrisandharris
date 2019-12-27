447803b051f777e5bc0e589e7a1582cb92ed03d9
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-27 15:07:42 -0800

    push

--- wp-content/themes/harris/page-templates/template-padirectory.php
+++ wp-content/themes/harris/page-templates/template-padirectory.php
@@ -4,9 +4,31 @@
 
 get_header(); ?>
 
+<div id="internal_main">
+
+
+<div class="internal_container">
+	
+	<div class="pa_directory_wrapper">
+		
+			<h1 class="internal_header cr_header no_bar"><?php the_title();?></h1>
+			
+			<div class="pa_inner_directory">
+		
+				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		
+			</div><!-- pa_inner_directory -->
+		
+	</div><!-- pa_directory_wrapper -->
+	
+</div><!-- internal_container -->
+
+
+</div><!-- internal_main -->
+						
 
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		
 
 
 <?php get_footer(); ?>
