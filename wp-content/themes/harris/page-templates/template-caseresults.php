b40ad76beaa93103784d83baf431d3de274cc983
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-27 09:46:19 -0800

    push

--- wp-content/themes/harris/page-templates/template-caseresults.php
+++ wp-content/themes/harris/page-templates/template-caseresults.php
@@ -4,7 +4,114 @@
 
 get_header(); ?>
 
+<div class="cr_internal_block"></div><!-- cr_internal_block -->
+	
+<img class="cr_internal_wave" src="<?php bloginfo('template_directory');?>/images/test-bg-bottom.svg"/>
 
+<div id="internal_main">
+
+<div class="internal_container">
+	
+	<h1 class="internal_header center cr_header"><?php the_title();?></h1>
+	
+	<div class="cr_internal_wrapper">
+		
+		<div class="single_cr_internal">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/cr-money-01.svg"/>
+			
+			<span class="amount">$4.95M</span>
+			
+			<span class="type">Motor Vehicle Accidents</span><!-- type -->
+			
+			<div class="description">
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+			</div><!-- description -->
+			
+		</div><!-- single_cr_internal -->
+		
+		<div class="single_cr_internal">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/cr-money-01.svg"/>
+			
+			<span class="amount">$4.95M</span>
+			
+			<span class="type">Motor Vehicle Accidents</span><!-- type -->
+			
+			<div class="description">
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+			</div><!-- description -->
+			
+		</div><!-- single_cr_internal -->
+		
+		<div class="single_cr_internal">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/cr-money-01.svg"/>
+			
+			<span class="amount">$4.95M</span>
+			
+			<span class="type">Motor Vehicle Accidents</span><!-- type -->
+			
+			<div class="description">
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+			</div><!-- description -->
+			
+		</div><!-- single_cr_internal -->
+		
+		<div class="single_cr_internal">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/cr-money-01.svg"/>
+			
+			<span class="amount">$4.95M</span>
+			
+			<span class="type">Motor Vehicle Accidents</span><!-- type -->
+			
+			<div class="description">
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+			</div><!-- description -->
+			
+		</div><!-- single_cr_internal -->
+		
+		<div class="single_cr_internal">
+			
+			<img src="<?php bloginfo('template_directory');?>/images/cr-money-01.svg"/>
+			
+			<span class="amount">$4.95M</span>
+			
+			<span class="type">Motor Vehicle Accidents</span><!-- type -->
+			
+			<div class="description">
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+				<p>Client sustained injuries to his neck, back, and shoulder after being t-boned by a utility company truck. His injuries required multiple surgeries.</p>
+				
+			</div><!-- description -->
+			
+		</div><!-- single_cr_internal -->
+		
+	</div><!-- cr_internal_wrapper -->
+	
+</div><!-- internal_container -->
+
+
+</div><!-- internal_main -->
 						
 			  
 
