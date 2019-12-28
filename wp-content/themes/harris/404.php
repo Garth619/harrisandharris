abf14e7944d90ebe820b43960ae32e1907b6bed0
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-27 18:17:06 -0800

    push

--- wp-content/themes/harris/404.php
+++ wp-content/themes/harris/404.php
@@ -7,16 +7,15 @@
 	
 	<div class="content_left content">
 		
-		<h1 class="large_internal_header">404 Error<br/> Page Not Found</h1><!-- large_internal_header -->
+		<h1 class="large_internal_header"><?php the_field( 'not_found_title','option'); ?></h1><!-- large_internal_header -->
 		
 		<div class="not_found_content">
 		
-			<p>Uh oh! The page you requested for could not be found. The link to this address may be broken or may have moved since your last visit.</p>
+			<?php the_field( 'not_found_content','option'); ?>
+			
 		</div><!-- not_found_content -->
 		
-		<span class="go_back">go back now!</span>
-		
-		<?php get_template_part( 'loop', 'page' ); ?>
+		<span class="go_back"><?php the_field( 'go_back_verbiage','option'); ?></span>
 				
 	</div><!-- content_left -->
 	
