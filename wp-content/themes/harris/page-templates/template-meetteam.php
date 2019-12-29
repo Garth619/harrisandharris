c66c1d3aa51b18dee393bedb374846691b75827c
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-28 17:54:22 -0800

    push

--- wp-content/themes/harris/page-templates/template-meetteam.php
+++ wp-content/themes/harris/page-templates/template-meetteam.php
@@ -14,6 +14,8 @@ get_header(); ?>
 	
 	<div class="meet_team_wrapper">
 		
+		<div class="meet_team_section">
+		
 		<span class="meet_team_header">Meet the Attorneys</span><!-- meet_team_header -->
 		
 		<div class="meet_team_inner">
@@ -119,6 +121,118 @@ get_header(); ?>
 			</div><!-- single_att -->
 			
 		</div><!-- meet_team_inner -->
+		
+		</div><!-- meet_team_section -->
+		
+		<div class="meet_team_section">
+		
+		<span class="meet_team_header">Meet the Staff</span><!-- meet_team_header -->
+		
+		<div class="meet_team_inner">
+			
+			<div class="single_att">
+				
+				<a href="">
+				
+					<div class="single_att_img">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/att-03.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+					
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_title">Heather E. Harris</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+				
+					<div class="single_att_img">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/att-03.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+					
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_title">Heather E. Harris</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+				
+					<div class="single_att_img">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/att-03.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+					
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_title">Heather E. Harris</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+				
+					<div class="single_att_img">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/att-03.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+					
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_title">Heather E. Harris</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a href="">
+				
+					<div class="single_att_img">
+					
+						<img src="<?php bloginfo('template_directory');?>/images/att-03.jpg"/>
+						
+						<div class="att_overlay"></div><!-- att_overlay -->
+					
+					</div><!-- single_att_img -->
+					
+					<span class="single_att_title">Heather E. Harris</span><!-- single_att_title -->
+					
+					<span class="single_att_position">Managing Partner</span><!-- single_att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+		</div><!-- meet_team_inner -->
+		
+		</div><!-- meet_team_section -->
 		
 	</div><!-- meet_team_wrapper -->
 	
