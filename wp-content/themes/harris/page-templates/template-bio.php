7c3ae2595629d6cd8a6bc606ddc875d936be1748
Garrett Cullen <Garth619@users.noreply.github.com>
2019-12-31 10:04:19 -0800

    push

--- wp-content/themes/harris/page-templates/template-bio.php
+++ wp-content/themes/harris/page-templates/template-bio.php
@@ -7,13 +7,31 @@ get_header(); ?>
 
 <div id="internal_main">
 	
-	<div class="cr_internal_block"></div><!-- cr_internal_block -->
+	<div class="bio_internal_block"></div><!-- cr_internal_block -->
 	
-	<img class="cr_internal_wave" src="<?php bloginfo('template_directory');?>/images/test-bg-bottom.svg"/>
+	<img class="bio_internal_wave" src="<?php bloginfo('template_directory');?>/images/bio-temp.jpg"/>
 
 	<div class="bio_content two_col">
 	
-	
+		<div class="bio_left">
+			
+			<div class="bio_img_wrapper">
+				
+				<img src="<?php bloginfo('template_directory');?>/images/att-bio-1.jpg"/>
+				
+			</div><!-- bio_img_wrapper -->
+			
+		</div><!-- bio_left -->
+		
+		<div class="bio_right">
+			
+			<h1 class="bio_header"><?php the_title();?></h1>
+			
+			<span class="bio_position">Managing Partner</span><!-- bio_position -->
+			
+			<span class="bio_description">If you have been injured in Nevada, as a resident or a guest, Brian is the right partner to help you recover compensation.</span><!-- bio_description -->
+			
+		</div><!-- bio_right -->
 	
 	</div><!-- bio_content -->
 
